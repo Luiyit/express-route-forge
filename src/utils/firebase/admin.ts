9b822890c147ae2d03b42a230d073107cf347845
Luiyit <luiyit.hernandez@gmail.com>
2025-11-18 07:50:09 +0100

    Validate existing firebase app

--- src/utils/firebase/admin.ts
+++ src/utils/firebase/admin.ts
@@ -1,17 +1,19 @@
 import admin from 'firebase-admin';
 
 export default function initWithServiceAccount(credentials: admin.ServiceAccount) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: credentials.projectId,
-      privateKey: (credentials.privateKey || '').replace(
-        /\\n/g,
-        '\n',
-      ),
-      clientEmail: credentials.clientEmail,
-    }),
-  });
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: credentials.projectId,
+        privateKey: (credentials.privateKey || '').replace(
+          /\\n/g,
+          '\n',
+        ),
+        clientEmail: credentials.clientEmail,
+      }),
+    });
+    
+    return admin;  
+  }
 
-  admin.auth
-  return admin;
 }
