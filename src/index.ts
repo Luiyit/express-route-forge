ed8cbe94400391c282cfb0704aa88627ec6a7a1e
Luiyit <luiyit.hernandez@gmail.com>
2025-11-25 16:26:23 +0100

    Exclude firebase from barrer utils file

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from './errors/index';
 export * from './express/index';
 export * from './types/index';
 export * from './utils/index';
+export { default as initFbWithServiceAccount } from './utils/firebase/admin';
 export * from './middlewares/index';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
-export { default as initFbWithServiceAccount } from './firebase/admin';
 export { default as validateSchema } from './validateJoiSchema';
 export { default as requireDirectoryFiles } from './requireDirectoryFiles';
 export { default as getPackageJson } from './packageJson';
